be83554bb85329b22b4b74aa5d018f1e78ecdfd5
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-04-25 15:19:45 +0300

    open/close catalog

--- main.js
+++ main.js
@@ -1,53 +1,80 @@
-const bigButton = document.querySelector('.big-button');
-const bigButtonPic = document.querySelector('.big-button__pic');
-const catalog = document.querySelector('.catalog');
-const mainElement = document.querySelector('article.main');
+const bigButton = document.querySelector(".big-button");
+const bigButtonPic = document.querySelector(".big-button__pic");
+const catalog = document.querySelector(".catalog");
+const mainElement = document.querySelector("article.main");
 
-bigButton.addEventListener('click', () => {
-    const display = getComputedStyle(catalog).display;
-    if (display !== 'none') {
-        catalog.style.opacity = '0'
-        setTimeout(() => {
-            mainElement.style.gridTemplateColumns = '1fr';
-            mainElement.style.gridTemplateAreas = '"general" "brands"';
-            catalog.style.display = 'none';
-            bigButtonPic.style.transform = 'rotate(0deg)';
-        }, 800)
-    } else {
-        catalog.style.display = 'flex';
-        setTimeout(() => { catalog.style.opacity = '1' }, 0)
-        mainElement.style.gridTemplateColumns = '';
-        mainElement.style.gridTemplateAreas = '';
-        bigButtonPic.style.transform = 'rotate(90deg)';
+const STEP = 4;
+const MAX = 252;
+
+const gridLeftHide = (cb) => {
+  const gtc = getComputedStyle(mainElement).gridTemplateColumns;
+
+  let gtcSplited = gtc.split(" ");
+  let gtcValue = gtcSplited.length > 1 ? parseInt(gtcSplited[0]) : 0;
+
+  const interval = setInterval(() => {
+    mainElement.style.gridTemplateColumns = `${gtcValue}px auto`;
+    if (gtcValue <= 0) {
+      cb();
+      clearInterval(interval);
     }
+    gtcValue -= STEP;
+  }, 0);
+};
+
+const gridLeftShow = (cb) => {
+  let gtcValue = 0;
+
+  const interval = setInterval(() => {
+    mainElement.style.gridTemplateColumns = `${gtcValue}px auto`;
+    if (gtcValue >= MAX) {
+      cb();
+      clearInterval(interval);
+    }
+    gtcValue += STEP;
+  }, 0);
+};
+
+bigButton.addEventListener("click", () => {
+  const opacity = getComputedStyle(catalog).opacity;
+  if (opacity != 0) {
+    catalog.style.opacity = "0";
+    gridLeftHide(() => {
+      setTimeout(() => {}, 0);
+    });
+  } else {
+    gridLeftShow(() => {
+      catalog.style.opacity = "1";
+    });
+  }
 });
 //--- MODAL ---
-const burgerButton = document.querySelector('.burger__pic');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.modal__close');
-burgerButton.addEventListener('click', () => {
-    const isOpen = getComputedStyle(modal).display;
-    console.log('🚀 ~ isOpen', isOpen, modal);
-    if (isOpen === 'none') {
-        modal.style.display = 'block';
-        document.body.style.overflow = 'hidden';
-    }
+const burgerButton = document.querySelector(".burger__pic");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".modal__close");
+burgerButton.addEventListener("click", () => {
+  const isOpen = getComputedStyle(modal).display;
+  console.log("🚀 ~ isOpen", isOpen, modal);
+  if (isOpen === "none") {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
 });
-close.addEventListener('click', () => {
-    document.body.style.overflow = '';
-    modal.style.display = 'none';
+close.addEventListener("click", () => {
+  document.body.style.overflow = "";
+  modal.style.display = "none";
 });
 //--- TOOGLE CATALOG__ITEM ---
-catalog.addEventListener('click', ({ target }) => {
-    const submenuContainer = target.closest('.sub-menu__container');
-    const submenuContent = submenuContainer.querySelector('.sub-menu__content');
-    console.log('🚀 ~ submenu', submenuContainer, submenuContent);
-    if (submenuContainer && submenuContent) {
-        const isOpen = getComputedStyle(submenuContent).display;
-        if (isOpen !== 'none') {
-            submenuContent.style.display = '';
-        } else {
-            submenuContent.style.display = 'block';
-        }
+catalog.addEventListener("click", ({ target }) => {
+  const submenuContainer = target.closest(".sub-menu__container");
+  const submenuContent = submenuContainer.querySelector(".sub-menu__content");
+  console.log("🚀 ~ submenu", submenuContainer, submenuContent);
+  if (submenuContainer && submenuContent) {
+    const isOpen = getComputedStyle(submenuContent).display;
+    if (isOpen !== "none") {
+      submenuContent.style.display = "";
+    } else {
+      submenuContent.style.display = "block";
     }
+  }
 });
