a2ca6a7497d32c90fa84ef2df4fd1d6eb51e29d3
voskanvl <voskanvl@gmail.com>
2022-04-27 06:35:03 +0300

    cancel scroll

--- slider.js
+++ slider.js
@@ -11,12 +11,12 @@ class Slider {
     #maxOffset;
     currentOffset = 0;
     els = null;
-    viewport = null;
+    wraper = null;
     min = true;
     max = false;
-    constructor(els, viewport, option) {
+    constructor(els, wraper, option) {
         this.els = [...els];
-        this.viewport = viewport;
+        this.wraper = wraper;
         this.currentCardId = 0;
         this.inc = this.#inc.bind(this);
         this.dec = this.#dec.bind(this);
@@ -25,8 +25,15 @@ class Slider {
             const gcs = getComputedStyle(el);
             return acc + parseInt(gcs.width) + parseInt(gcs.marginRight);
         }, 0);
-        this.#maxOffset = this.#commonWidth - viewport.clientWidth;
-        console.log(viewport, this.#commonWidth, this.#maxOffset);
+        this.#maxOffset = this.#commonWidth - wraper.clientWidth;
+        //запрещаем прокрутку
+        ["scroll", "wheel", "touchmove"].forEach(type =>
+            this.wraper.parentElement.addEventListener(type, ev => {
+                // console.log("scroll", this);
+                // this.#currentCardId = this.getCurrentCard();
+                ev.preventDefault();
+            }),
+        );
     }
     get currentCardId() {
         return this.#currentCardId;
@@ -34,7 +41,7 @@ class Slider {
     set currentCardId(x) {
         if (x >= this.els.length) {
             this.max = true;
-            this.viewport.dispatchEvent(this.#edgeOver);
+            this.wraper.dispatchEvent(this.#edgeOver);
             return;
         } else {
             this.max = false;
@@ -42,7 +49,7 @@ class Slider {
         }
         if (x <= 0) {
             this.min = true;
-            this.viewport.dispatchEvent(this.#edgeUnder);
+            this.wraper.dispatchEvent(this.#edgeUnder);
             return;
         } else {
             this.min = false;
@@ -51,10 +58,12 @@ class Slider {
         console.log("min -", this.min, "max -", this.max);
     }
     #inc() {
+        // this.#currentCardId = this.getCurrentCard();
         this.currentCardId++;
         this.#moveToNextCard();
     }
     #dec() {
+        // this.#currentCardId = this.getCurrentCard();
         this.currentCardId--;
         this.#moveToNextCard();
     }
@@ -63,13 +72,29 @@ class Slider {
             this.els[this.currentCardId].getBoundingClientRect();
         const elementLeft = elementRect.x;
 
-        const viewportRect = this.viewport.getBoundingClientRect();
-        const viewportLeft = viewportRect.x;
+        const wraperRect = this.wraper.getBoundingClientRect();
+        const wraperLeft = wraperRect.x;
 
-        const offset = viewportLeft - elementLeft;
+        const offset = wraperLeft - elementLeft;
 
-        this.viewport.style.transform = `translateX(${offset}px)`;
+        this.wraper.style.transform = `translateX(${offset}px)`;
         this.currentOffset = offset;
         console.log("🚀 ~ this.currentOffset", offset, this.currentOffset);
+    }
+    getCurrentCard() {
+        //определяем смещение vieport scroll = this.wraper.parentElement.scrollLeft
+        //вычесляем на какой элемент попадант scroll
+        const scroll = this.wraper.parentElement.scrollLeft;
+        let currentWidth = 0;
+        let currentElement = -1;
+        for (let idx in this.els) {
+            const widthEl = this.els[idx].getBoundingClientRect().width;
+            if (scroll >= currentWidth && scroll < currentWidth + widthEl) {
+                currentElement = idx;
+                break;
+            }
+            currentWidth += widthEl;
+        }
+        return +currentElement;
     }
 }
