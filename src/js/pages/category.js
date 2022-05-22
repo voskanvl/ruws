6bcb85e80d7fa981fe9427e9a236e8ef6a81ff1b
voskanvl <voskanvl@gmail.com>
2022-05-22 20:52:22 +0300

    start category

--- src/js/pages/category.js
+++ src/js/pages/category.js
@@ -1,1 +1,21 @@
 import "../../styles/style.sass";
+
+if (document.readyState === "loading") {
+    console.log('document.readyState === "loading"');
+    document.addEventListener("DOMContentLoaded", start);
+} else {
+    console.log('document.readyState !== "loading"');
+    start();
+}
+function start() {
+    const catalog = document.querySelector(".catalog");
+    //--- TOOGLE CATALOG__ITEM ---
+    catalog.addEventListener("click", ({ target }) => {
+        const subMenu = target
+            .closest(".catalog__item")
+            .querySelector(".sub-menu__content");
+        if (subMenu) {
+            subMenu.classList.toggle("sub-menu__content_show");
+        }
+    });
+}
