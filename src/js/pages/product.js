87941c74020a0ac1ae8782da4b5f1a6c34e5803f
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-05-25 16:55:46 +0300

    insert splide

--- src/js/pages/product.js
+++ src/js/pages/product.js
@@ -1,6 +1,8 @@
 import "../../styles/style.sass";
 // import "../../components/range/range";
 import "../bigButtonListener";
+import "../switchTabs";
+import Splide from "@splidejs/splide";
 
 if (document.readyState === "loading") {
     console.log('document.readyState === "loading"');
@@ -22,16 +24,34 @@ function start() {
     };
     catalog.addEventListener("click", clickHandler);
     //--- INIT CAROUSEL
-    M.Carousel.init(
-        document.querySelector(".product-resume__carousel.carousel"),
-        {
-            indicators: false,
-            numVisible: 3,
-            padding: 7,
-            fullWidth: true,
+    // M.Carousel.init(
+    //     document.querySelector(".product-resume__carousel.carousel"),
+    //     {
+    //         indicators: false,
+    //         numVisible: 3,
+    //         padding: 7,
+    //         fullWidth: true,
+    //     },
+    // );
+    new Splide(".splide", {
+        rewind: true,
+        fixedWidth: 104,
+        fixedHeight: 58,
+        isNavigation: true,
+        gap: 10,
+        focus: "center",
+        pagination: false,
+        cover: true,
+        dragMinThreshold: {
+            mouse: 4,
+            touch: 10,
         },
-    );
-    //--- close Catalog
-    // catalog.style.width = "0";
-    // catalog.style.overflow = "hidden";
+        breakpoints: {
+            640: {
+                fixedWidth: 66,
+                fixedHeight: 38,
+            },
+        },
+    }).mount();
+    //--- INIT Tabs
 }
