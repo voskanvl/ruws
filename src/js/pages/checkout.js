6aca8ccb71a71c857ef49d56572c4d11b740de9c
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-06-03 11:28:32 +0300

    checkout done

--- src/js/pages/checkout.js
+++ src/js/pages/checkout.js
@@ -9,6 +9,16 @@ if (document.readyState === "loading") {
     start();
 }
 function start() {
+    const paymethdCheck = document.querySelector(".paymethod .checkbox");
     //--- CLICK BigBUTTON
     document.querySelector(".big-button").click();
+    paymethdCheck.parentElement.addEventListener("click", () => {
+        let { display } = paymethdCheck.style;
+        console.log("🚀 ~ display", display);
+        if (display === "none") {
+            paymethdCheck.style.display = "block";
+        } else {
+            paymethdCheck.style.display = "none";
+        }
+    });
 }
